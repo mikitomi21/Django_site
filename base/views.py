ac4af44fde12a45300374f0143c39f921b7884ab
mikitomi21 <kubastach2001@gmail.com>
2023-02-26 18:07:17 +0100

    Load data from database

--- base/views.py
+++ base/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
+'''
 rooms = [
     {'id':1, 'name':'Python'},
     {'id':2, 'name':'C++'},
     {'id':3, 'name':'Matplotlib'},
     {'id':4, 'name':'Pandas'},
 ]
+'''
+
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
-    
+    room = Room.objects.get(id=pk)
     context = {'room':room}
-    
     return render(request, 'base/room.html', context)

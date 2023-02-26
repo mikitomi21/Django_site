e1a525bb67fbf94522659662e1df75ff2faa05d2
mikitomi21 <kubastach2001@gmail.com>
2023-02-26 17:19:38 +0100

    Create templates with including bar, extending main and passing dictonary of rooms

--- base/views.py
+++ base/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name':'Python'},
+    {'id':2, 'name':'C++'},
+    {'id':3, 'name':'Matplotlib'},
+    {'id':4, 'name':'Pandas'},
+]
 
 def home(request):
-    return HttpResponse('Hello')
+    context = {'rooms':rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
-    return HttpResponse('Room')
+    return render(request, 'room.html')
